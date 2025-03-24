12e503685cfcb5756c8f4359ded97649f7b79cda
nicol <nicolascasella02@gmail.com>
2025-03-24 14:24:48 -0300

    refactor: modified best_first_search node frontier check

--- search-methods/src/search_methods.py
+++ search-methods/src/search_methods.py
@@ -11,10 +11,8 @@ from result import Result
 def depth_first_search(problem:Problem) -> Result:
     return limited_depth_first_search(problem=problem,limit=math.inf)
 
-
 def limited_depth_first_search(problem:Problem,limit:int) -> Result:#STACK
     return _general_search(problem=problem,limit=limit,collection=[])
-
 
 def breath_first_search(problem:Problem) -> Result:#COLA
     return _general_search(problem=problem,limit=math.inf,collection=deque())
@@ -44,16 +42,15 @@ def _general_search(problem:Problem,limit:int,collection:Iterable) -> Result:
     end_time=time.time()
     return Result(success=False,nodes_expanded=nodes_expanded,processing_time=end_time-start_time)
 
-
 def best_first_search(problem:Problem,f:tuple[Callable,Callable]) -> Result:
-    def comparator_nodes(node1,node2):
+    def node_comparator(node1,node2):
         primary_diff=f[0](node1)-f[0](node2)
         if primary_diff!=0:
             return primary_diff
         return f[1](node1)-f[1](node2) if len(f)>1 else 0
 
     node:Node=Node(state=problem.initial_state)
-    fr=SortedList(key=cmp_to_key(comparator_nodes))
+    fr=SortedList(key=cmp_to_key(node_comparator))
     start_time=time.time()
     fr.add(node)
     explored_states=set()
@@ -67,19 +64,17 @@ def best_first_search(problem:Problem,f:tuple[Callable,Callable]) -> Result:
         for action in problem.get_actions(node.state):
             child=node.generate_child_node(problem=problem,action=action)
             nodes_expanded+=1
-            if child.state not in explored_states and child not in fr:
+            try:
+                child_index=fr.index(child)
+            except ValueError:
+                child_index=None
+            if child.state not in explored_states and child_index is None:
                 fr.add(child)
-            else:
-                try:
-                    child_index=fr.index(node)
-                except ValueError:
-                    child_index=None
-                if child_index is not None and f[0](child)<f[0](fr[child_index]):
-                    fr.pop(child_index)
-                    fr.add(child)
+            elif child_index is not None and f[0](child)<f[0](fr[child_index]):
+                fr.pop(child_index)
+                fr.add(child)
     end_time=time.time()
     return Result(success=False,nodes_expanded=nodes_expanded,processing_time=end_time-start_time)
-
 
 def a_star_search(problem:Problem,h:Callable) -> Result:
     return best_first_search(problem=problem,f=(lambda node:node.cost+h(node),h) )
