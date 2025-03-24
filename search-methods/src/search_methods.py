7135de0df5174326fc89814ff1b60b2fed286f7d
nicol <nicolascasella02@gmail.com>
2025-03-24 11:35:19 -0300

    refactor: changed dfs implementation to iterative method

--- search-methods/src/search_methods.py
+++ search-methods/src/search_methods.py
@@ -1,33 +1,41 @@
-from problem_def import *
 from collections import deque
+import math
 import time
+from functools import cmp_to_key
 from _collections_abc import Callable
 from sortedcontainers.sortedlist import SortedList
-from functools import cmp_to_key
+from problem_def import Problem,Node
 from result import Result
-_LIMIT_REACHED_STRING="Limit reached"
-_NO_SOLUTION_FOUND_STRING="No solution present"
 
-def depth_limited_search(problem:Problem,limit:int) -> Result:
-    root_node:Node=Node(state=problem.initial_state)
-    """Devuelve tupla con resultado de busqueda. si primer valor es true, el segundo valor devuelve cola de secuencia de acciones, sino, devuelve razon de fallo"""
-    return _recursive_depth_limited_search(node=root_node, limit=limit)
 
-def _recursive_depth_limited_search(node:Node,problem:Problem,limit:int) -> Result:
+def depth_first_search(problem:Problem) -> Result:
+    return limited_depth_first_search(problem=problem,limit=math.inf)
+
+#TODO: ver si es necesario agregar un set de estados repetidos para busqueda de arbol
+def limited_depth_first_search(problem:Problem,limit:int) -> Result:#LIFO
+    start_time=time.time()
+    node:Node=Node(problem.initial_state)
+    fr=[]
+    fr.append(node)
     if problem.is_goal_state(node.state):
-        return (True,node.get_action_sequence_to_root())
-    if limit==0:
-        return (False,_LIMIT_REACHED_STRING)
-    reached_limit=False
-    for action in problem.get_actions(state=node.state):
-        child=node.generate_child_node(problem=problem,action=action)
-        result=_recursive_depth_limited_search(node=child,problem=problem,limit=limit-1)
-        reached_limit=result[1]==_LIMIT_REACHED_STRING
-        if result[0]:
-            return result
-    return (False,_LIMIT_REACHED_STRING) if reached_limit else (False,_NO_SOLUTION_FOUND_STRING)
+        end_time=time.time()
+        return Result(success=True,result_cost=node.cost,solution=node.get_action_sequence_to_root(),processing_time=end_time-start_time)
+    nodes_expanded=0
+    while len(fr)!=0 and limit!=0:
+        node=fr.pop()
+        if problem.is_goal_state(node.state):
+            end_time=time.time()
+            return Result(success=True,result_cost=node.cost,nodes_frontier=len(fr),nodes_expanded=nodes_expanded,solution=node.get_action_sequence_to_root(),processing_time=end_time-start_time)
+        for action in problem.get_actions(node.state):
+            child=node.generate_child_node(problem=problem,action=action)
+            nodes_expanded+=1
+            fr.append(child)
+        limit-=1
+    end_time=time.time()
+    return Result(success=False,nodes_expanded=nodes_expanded,processing_time=end_time-start_time)
 
-def breath_first_search(problem:Problem) -> Result:
+
+def breath_first_search(problem:Problem) -> Result:#FIFO
     start_time=time.time()
     node:Node=Node(problem.initial_state)
     fr=deque()
@@ -45,7 +53,8 @@ def breath_first_search(problem:Problem) -> Result:
             child=node.generate_child_node(problem=problem,action=action)
             nodes_expanded+=1
             fr.append(child)
-    return Result()
+    end_time=time.time()
+    return Result(success=False,nodes_expanded=nodes_expanded,processing_time=end_time-start_time)
 
 def best_first_search(problem:Problem,f:tuple[Callable,Callable]) -> Result:
     def comparator_nodes(node1,node2):
@@ -79,7 +88,9 @@ def best_first_search(problem:Problem,f:tuple[Callable,Callable]) -> Result:
                 if child_index is not None and f[0](child)<f[0](fr[child_index]):
                     fr.pop(child_index)
                     fr.append(child)
-    return None
+    end_time=time.time()
+    return Result(success=False,nodes_expanded=nodes_expanded,processing_time=end_time-start_time)
+
 
 def a_star_search(problem:Problem,h:Callable) -> Result:
     return best_first_search(problem=problem,f=(lambda node:node.cost+h(node),h) )
