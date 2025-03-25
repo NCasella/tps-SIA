e7156f44fdc8da9a2272a5659b54e8c18dbb109c
nicol <nicolascasella02@gmail.com>
2025-03-25 10:23:16 -0300

    feat: initial sokoban implementation

--- search-methods/src/sokoban.py
+++ search-methods/src/sokoban.py
@@ -1,17 +1,34 @@
 from problem_def import Problem
 from enum import Enum
 
-class BlockType(Enum):
-    WALL=0
-    BOX=1
-    OBJECTIVE=2
-    EMPTYNESS=3
+
+_WALL='#'
+_BOX='$'
+_OBJECTIVE='.'
+_EMPTYNESS=' '
+_PLAYER='@'
+
+class Movement(Enum):
+    UP=(1,0)
+    DOWN=(-1,0)
+    LEFT=(0,-1)
+    RIGHT=(0,1)
 
 
 class Sokoban(Problem):
-    def __init__(self, initial_state, goal_state):
-        super().__init__(initial_state=initial_state,goal_state= goal_state)
+    def __init__(self, initial_state):
+        super().__init__(initial_state=initial_state)
+        self.objective_positions=set()
+        for i in range(initial_state):
+            for j in range(initial_state[i]):
+                if initial_state[i][j]==_OBJECTIVE:
+                    self.objective_positions.add((i,j))
 
     def get_actions(self,state):
-        return super().get_actions(state=state)
-    
+        pass
+
+    def get_state_result(self,state,action):
+        pass
+
+    def get_cost_to_state(self,start_state,action,end_state):
+        return 1
