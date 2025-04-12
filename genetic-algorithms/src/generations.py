d0b0c2fa46db4f301336677ff5b1abd391a7955f
NCasella <nicolascasella02@gmail.com>
2025-04-12 11:32:06 -0300

    feat: added missing generation criteria method

--- genetic-algorithms/src/generations.py
+++ genetic-algorithms/src/generations.py
@@ -3,7 +3,13 @@ from src.config import config
 import random
 
 def _young_next_generation(current_population: list[Individual], children: list[Individual]) -> list[Individual]:
-    pass
+    K:int=config["selection_amount"]
+    N:int =config["population"]
+    if K>N:
+        return random.choices(children,K=N)
+    selected_current_population=random.choices(current_population,k=N-K)
+    selected_new_generation=random.choices(children,k=N)
+    return selected_current_population+selected_new_generation
 
 def _traditional_next_generation(current_population: list[Individual], children: list[Individual]) -> list[Individual]:
     generation_gap: float = config["generation_gap"]
@@ -20,6 +26,6 @@ def next_generation(current_population: list[Individual], children: list[Individ
     next_generation_criteria: str = config["criteria"]
     criteria_methods: dict[str, callable] = {
         "traditional": _traditional_next_generation,
-        "young": lambda: None,
+        "young": _young_next_generation,
     }
     return criteria_methods[next_generation_criteria](current_population, children)
