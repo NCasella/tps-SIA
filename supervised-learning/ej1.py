2c0fbbdb6fac266aa0d0583eb22903e7d5d9c802
NCasella <nicolascasella02@gmail.com>
2025-04-30 12:48:34 -0300

    refactor: changed hyperparameters

--- supervised-learning/ej1.py
+++ supervised-learning/ej1.py
@@ -5,7 +5,7 @@ if __name__=="__main__":
     and_output = (-1, -1, -1, 1)
     xor_output = (-1, 1, 1, -1)
     perc=SimplePerceptron(5e-2,dataset,and_output)
-    perc.train_perceptron(10000000,2e-4)
+    perc.train_perceptron(1000,2e-2)
     print(perc.predict_output((-1, -1)))
     print(perc.predict_output((-1, 1)))
     print(perc.predict_output((1, -1)))
