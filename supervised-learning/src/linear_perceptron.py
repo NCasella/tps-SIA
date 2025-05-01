da66f11b0c9fc66655fb21df44c74dfdc03c3206
NCasella <nicolascasella02@gmail.com>
2025-05-01 14:54:47 -0300

    feat: implemented calculate_error to Linear Perceptron

--- supervised-learning/src/linear_perceptron.py
+++ supervised-learning/src/linear_perceptron.py
@@ -5,5 +5,5 @@ class LinearPerceptron(SimplePerceptron):
     def compute_activation(hμ):
         return hμ
     
-    def calculate_error(self, x_set):
-        raise NotImplementedError
+    def calculate_error(self,expected, output):
+        return 0.5*((expected-output))**2
