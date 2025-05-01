f72b0e2c3bfb896dcad6923258d11c6e8911b6aa
NCasella <nicolascasella02@gmail.com>
2025-05-01 18:49:22 -0300

    feat: added config file to ej1

--- supervised-learning/ej1.py
+++ supervised-learning/ej1.py
@@ -1,11 +1,24 @@
+import sys
+import json
 from src.simple_perceptron import SimplePerceptron
 
+dataset = ((-1,-1), (-1, 1), (1,-1), (1,1))
+and_output = (-1, -1, -1, 1)
+xor_output = (-1, 1, 1, -1)
+training_output_map={
+    "AND":and_output,
+    "XOR":xor_output
+    }    
+
 if __name__=="__main__":
-    dataset = ((-1,-1), (-1, 1), (1,-1), (1,1))
-    and_output = (-1, -1, -1, 1)
-    xor_output = (-1, 1, 1, -1)
-    perc=SimplePerceptron(5e-2,dataset,and_output)
-    perc.train_perceptron(1000,2e-2)
+    with open(sys.argv[1],"r") as file:
+        config=json.load(file)
+    learning_rate=config["learning_rate"]
+    epsilon=config["epsilon"]
+    function=config["function"]
+    epochs=config["epochs"]
+    perc=SimplePerceptron(learning_rate=learning_rate,training_input=dataset,training_output=training_output_map[function])
+    perc.train_perceptron(epochs=epochs,epsilon=epsilon)
     print(perc.predict_output((-1, -1)))
     print(perc.predict_output((-1, 1)))
     print(perc.predict_output((1, -1)))
