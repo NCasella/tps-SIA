d0d85df2cde90c1134c1b76857c5283ab3fe88d6
NCasella <nicolascasella02@gmail.com>
2025-05-08 15:37:02 -0300

    feat: added optimizer and activations history

--- supervised-learning/src/multilayer_perceptron.py
+++ supervised-learning/src/multilayer_perceptron.py
@@ -1,11 +1,13 @@
 import numpy as np
 from src.non_linear_perceptron import NonLinearPerceptron
+from src.optimizers.optimizer import Optimizer
 
 class MultilayerPerceptron(NonLinearPerceptron):
-   def __init__(self,learning_rate:float,training_input:list,training_output: list,activation_function:callable,activation_function_derivate:callable,layers_structure:list[int]):
+   def __init__(self,learning_rate:float,training_input:list,training_output: list,activation_function:callable,activation_function_derivate:callable,layers_structure:list[int],optimizer:Optimizer):
       super().__init__(learning_rate,training_input,training_output,activation_function,activation_function_derivate)
       self.layers_structure=layers_structure
       self.weights=[]
+      self.optimizer=optimizer
       self.weights.append(np.random.randn(self.training_input.shape[1],layers_structure[0]))
       for i in range(len(layers_structure)-1):
          self.weights.append(np.random.randn((layers_structure[i],layers_structure[i+1])) ) 
@@ -14,7 +16,7 @@ class MultilayerPerceptron(NonLinearPerceptron):
       for epoch in range(epochs):
          total_error = 0
          for Xμ in self.training_input:
-            hμ=self.predict_output(Xμ)
+            hμ,activations =self.predict_output(Xμ)
             #TODO
          if total_error<epsilon:
             print(f"Convergencia en epoch {epoch}")
@@ -26,12 +28,12 @@ class MultilayerPerceptron(NonLinearPerceptron):
       print("No convergencia :(")
    
    #feedfoward (?
-   def predict_output(self, input)->np.array:
+   def predict_output(self, input)->tuple[np.array, list[np.array]]:
       h_o=np.vectorize(self.activation_function)
-      output=input
+      outputs=[input]
       for weight_matrix in self.weights:
-         output=h_o(np.matmul(output,weight_matrix))
-      return output
+         outputs.append(h_o(np.matmul(outputs[-1],weight_matrix)))
+      return outputs[-1],outputs
    
    def _backtrack(self,output,expected)->list[np.matrix]:
       raise NotImplementedError
