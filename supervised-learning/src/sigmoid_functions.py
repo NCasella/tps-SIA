445d16e8c968dd99f01d17a6a55861cca2462bce
mquesada02 <mquesada@itba.edu.ar>
2025-05-11 15:55:19 -0300

    chore: adapted functions for vectors as well

--- supervised-learning/src/sigmoid_functions.py
+++ supervised-learning/src/sigmoid_functions.py
@@ -1,32 +1,42 @@
-import math
 import numpy as np
 
-def _tanh(beta,h):
-    return np.tanh(beta*h)
-def _logistic(beta,h):
-    return 1/(1+math.exp(-2*beta*h))
-def _relu(beta,h):
-    return max(0,h)
-def _softplus(beta,h):
-    return math.log(1+math.exp(h))
+def _tanh(beta, h):
+    return np.tanh(beta * h)
 
-def _tanh_derivate(beta,h):
-    return beta*(1-_tanh(beta,h)**2.0)
-def _logistic_derivate(beta,h):
-    return 2*beta*_logistic(beta,h)*(1-_logistic(beta,h))
-def _relu_derivate(beta,h):
-    return 1 if h>0 else 0
-def _softplus_derivate(beta,h):
-    return math.exp(h)/(1+math.exp(h))
+def _logistic(beta, h):
+    return 1 / (1 + np.exp(-2 * beta * h))
 
-_sigmoid_functions:dict[str,tuple[callable,callable]]={
-        "tanh":(_tanh,_tanh_derivate),
-        "logistic":(_logistic,_logistic_derivate),
-        "relu":(_relu,_relu_derivate),
-        "softplus": (_softplus,_softplus_derivate)
+def _relu(beta, h):
+    return np.maximum(0, h)
+
+def _softplus(beta, h):
+    return np.log(1 + np.exp(h))
+
+def _tanh_derivate(beta, h):
+    return beta * (1 - np.square(_tanh(beta, h)))
+
+def _logistic_derivate(beta, h):
+    sig = _logistic(beta, h)
+    return 2 * beta * sig * (1 - sig)
+
+def _relu_derivate(beta, h):
+    return np.where(h > 0, 1.0, 0.0)
+
+def _softplus_derivate(beta, h):
+    return np.exp(h) / (1 + np.exp(h))
+
+_sigmoid_functions: dict[str, tuple[callable, callable]] = {
+    "tanh": (_tanh, _tanh_derivate),
+    "logistic": (_logistic, _logistic_derivate),
+    "relu": (_relu, _relu_derivate),
+    "softplus": (_softplus, _softplus_derivate)
 }
 
-def get_sigmoid_function_and_derivate(beta,function):
-    sigmoid_function = lambda h: _sigmoid_functions[function][0](beta, h)
-    sigmoid_derivate = lambda h: _sigmoid_functions[function][1](beta, h)
+def get_sigmoid_function_and_derivate(beta, function):
+    def sigmoid_function(h):
+        return _sigmoid_functions[function][0](beta, h)
+
+    def sigmoid_derivate(h):
+        return _sigmoid_functions[function][1](beta, h)
+
     return sigmoid_function, sigmoid_derivate
