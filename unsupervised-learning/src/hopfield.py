b4d9a8200fedcfe740d63285505dc02a88c170db
NCasella <nicolascasella02@gmail.com>
2025-05-23 14:07:52 -0300

    feat: initial hopfield impl

---
+++ unsupervised-learning/src/hopfield.py
@@ -0,0 +1,25 @@
+import numpy as np 
+
+class Hopfield:
+    def __init__(self,input_data , weights=None):
+        self.input_data :np.ndarray =np.array(input_data)
+        if weights is not None:
+            self.weights :np.ndarray=np.array(weights)
+        else:
+            self.weights:np.ndarray= self.input_data.transpose()@self.input_data/self.input_data.shape[1]
+            np.fill_diagonal(self.weights,0)
+    
+    def predic_output(self, input, epochs):
+        current_state=np.array(input)
+        state_history=[current_state]
+        for epoch in range(epochs):
+            current_state=np.sign(current_state@self.weights)
+            state_history.append(current_state)
+        return state_history
+    
+    def _get_energy_(self, state):
+        sum=0
+        for i in range(self.weights.shape[0]):
+            for j in range(self.weights.shape[1]):
+                sum+=self.weights[i][j]*state[i]*state[j]
+        return -0.5*sum
