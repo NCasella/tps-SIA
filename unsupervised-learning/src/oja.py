cf47c2cc54ec9ab74774b731f40a79fbdd06650f
NCasella <nicolascasella02@gmail.com>
2025-05-24 14:08:20 -0300

    fixed: networks parameters

--- unsupervised-learning/src/oja.py
+++ unsupervised-learning/src/oja.py
@@ -1,9 +1,10 @@
 import numpy as np 
 
 class Oja:
-    def __init__(self, input_data:list, initial_learning_rate:float=1e-3, weights=None ):
+    def __init__(self, input_data:list, initial_learning_rate:float=0.5,constant_learning_rate=False, weights=None ):
         self.input_data=np.array(input_data)
-        self.learning_rate=initial_learning_rate
+        self.learning_rate=initial_learning_rate if not constant_learning_rate else 1e-3
+        self.constant_learning_rate=constant_learning_rate
         self.weights=weights if weights is not None else np.random.uniform(size=self.input_data.shape[1])
     
     def train_network(self, epochs):
@@ -12,5 +13,7 @@ class Oja:
                 output=self.input_data[µ]@self.weights
                 for i in range(len(self.weights)):
                     self.weights[i]+=self.learning_rate*output*(self.input_data[µ][i] -output*self.weights[i])
-            self.learning_rate=self.learning_rate/epoch if self.learning_rate !=1e-3 else self.learning_rate 
-            
+            self.learning_rate=self.learning_rate/epoch if not self.constant_learning_rate else self.learning_rate 
+    
+    def map_input(self, input):
+        return input@self.weights
