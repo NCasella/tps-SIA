8f6e4e7d4da649f3ddef8fcd76763982b631c954
NCasella <nicolascasella02@gmail.com>
2025-06-24 13:55:14 -0300

    added merge heatmap function

--- autoencoders/src/utils.py
+++ autoencoders/src/utils.py
@@ -47,3 +47,26 @@ def rgba_array_to_png(arr: np.ndarray, path: str, original_shape: Optional[Tuple
 
     img = Image.fromarray(arr, mode="RGBA")
     img.save(path)
+def save_merged_heatmaps(outputs, filename="merged.png", grid_shape=(6, 6), pad=2, threshold=0.5,binary=False):
+    n_images = len(outputs)
+    grid_h, grid_w = grid_shape
+
+    # Reshape and binarize each output to (7, 5)
+    reshaped_imgs = [(img.reshape((7, 5)) > threshold).astype(float) if binary else img.reshape((7,5)) for img in outputs]
+    padded_imgs = [np.pad(img, pad_width=pad, mode='constant', constant_values=0) for img in reshaped_imgs]
+    blank = np.zeros_like(padded_imgs[0])
+    padded_outputs = padded_imgs + [blank] * (grid_h * grid_w - n_images)
+
+    rows = []
+    for i in range(grid_h):
+        row_imgs = padded_outputs[i * grid_w:(i + 1) * grid_w]
+        row = np.concatenate(row_imgs, axis=1)
+        rows.append(row)
+    grid_img = np.concatenate(rows, axis=0)
+
+    plt.figure(figsize=(grid_w * 4, grid_h * 4))
+    plt.axis('off')
+    plt.imshow(grid_img, cmap='gray', vmin=0, vmax=1)
+    plt.savefig(filename, bbox_inches='tight', pad_inches=0.1)
+    plt.close()
+    
